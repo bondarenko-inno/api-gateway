ce38a28b5a0972a80a48e3fa845d7886be819ff0
bondarenko-inno <yahor.bandarenka@innowise.com>
2025-09-16 11:06:22 +0300

    cors fix

--- src/main/java/org/ebndrnk/apigateway/config/SecurityConfig.java
+++ src/main/java/org/ebndrnk/apigateway/config/SecurityConfig.java
@@ -65,14 +65,14 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.setAllowedOrigins(List.of("http://localhost:5173"));
-        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
-        configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type", "Cache-Control"));
+        configuration.setAllowedMethods(List.of("*"));
+        configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
-        configuration.setMaxAge(3600L);
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
         return source;
     }
+
 
 }
